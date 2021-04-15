bd5ae936cdf3ff80c0850e15ede8978789f4f361
Sophia Seremetis <shseremetis@gmail.com>
2021-04-15 17:21:36 -0400

    set up models

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const { ApolloServer } = require('apollo-server')
 const gql = require('graphql-tag')
 const mongoose = require('mongoose')
 
+const { MONGODB } = require('./config')
+
 const typeDefs = gql `
     type Query {
         sayHi: String!
@@ -19,9 +21,12 @@ const server = new ApolloServer({
     resolvers
 })
 
-mongoose.connect()
-
-server.listen({port: 5000})
+mongoose.connect(MONGODB, { useNewUrlParser: true })
+    .then(() => {
+        console.log('MongoDB Connected')
+        return server.listen({port: 5000})
+    })
     .then(res => {
         console.log(`Server running at ${res.url}`)
     })
+
