0609679427c35d63fd87dc7b384eb142d1a0874d
Sophia Seremetis <shseremetis@gmail.com>
2021-04-21 14:39:05 -0400

    add error message for invalid login

--- graphql/resolvers/users.js
+++ graphql/resolvers/users.js
@@ -6,8 +6,49 @@ const { validateRegisterInput, validateLoginInput } = require("../../util/valida
 const { SECRET_KEY } = require("../../config");
 const User = require("../../models/User");
 
+function generateToken(user){
+    return jwt.sign(
+        {
+          id: user.id,
+          email: user.email,
+          username: user.username,
+        },
+        SECRET_KEY,
+        { expiresIn: "1h" }
+      );
+}
+
 module.exports = {
   Mutation: {
+    async login(_, { username, password }){
+        const {errors, valid} = validateLoginInput(username, password)
+
+        if(!valid){
+          throw new UserInputError('Errors', { errors })
+        }
+
+        const user = await User.findOne({ username })
+        
+        if(!user){
+            errors.general = 'User not found'
+            throw new UserInputError('User not found', { errors })
+        }
+
+        const match = await bcrypt.compare(password, user.password)
+        if(!match){
+            errors.general = 'Wrong Credentials'
+            throw new UserInputError('Wrong Credentials', { errors })
+        }
+
+        const token = generateToken(user)
+
+        return {
+            ...user._doc,
+            id: user._id,
+            token,
+          };
+    },
+
     async register(
       _,
       { registerInput: { username, email, password, confirmPassword } }
@@ -44,15 +85,7 @@ module.exports = {
 
       const res = await newUser.save();
 
-      const token = jwt.sign(
-        {
-          id: res.id,
-          email: res.email,
-          username: res.username,
-        },
-        SECRET_KEY,
-        { expiresIn: "1h" }
-      );
+      const token = generateToken(res)
 
       return {
         ...res._doc,
