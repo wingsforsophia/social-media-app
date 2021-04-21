44e5359d516ed48b0a846aeb8c479edf12f89ace
Sophia Seremetis <shseremetis@gmail.com>
2021-04-21 13:19:25 -0400

    hash pws

---
+++ graphql/resolvers/users.js
@@ -0,0 +1,45 @@
+const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken');
+
+const { SECRET_KEY } = require('../../config')
+const User = require("../../models/User");
+
+module.exports = {
+  Mutation: {
+    async register(
+      _,
+      { 
+        registerInput: { username, email, password, confirmPassword }
+      },
+      context,
+      info
+    ) {
+      //TODO: validate user data
+      //todo: make sure user doesn't already exist
+      //todo: hash password and create an auth token
+      password = await bcrypt.hash(password, 12)
+
+      const newUser = new User({
+          email,
+          username, 
+          password, 
+          createdAt: new Date().toISOString()
+      })
+
+      const res = await newUser.save()
+
+      const token = jwt.sign({
+          id: res.id,
+          email: res.email, 
+          username: res.username
+      }, SECRET_KEY, { expiresIn: '1h'})
+
+      return {
+          ...res._doc, 
+          id: res._id, 
+          token
+      }
+
+    },
+  },
+};
