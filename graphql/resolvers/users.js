33bc9f37e3faea27596ef4a41e3afd146d5eafb6
Sophia Seremetis <shseremetis@gmail.com>
2021-04-21 13:24:04 -0400

    adds error for duplicate usernames

--- graphql/resolvers/users.js
+++ graphql/resolvers/users.js
@@ -1,5 +1,6 @@
 const bcrypt = require('bcryptjs')
 const jwt = require('jsonwebtoken');
+const { UserInputError } = require('apollo-server')
 
 const { SECRET_KEY } = require('../../config')
 const User = require("../../models/User");
@@ -11,11 +12,18 @@ module.exports = {
       { 
         registerInput: { username, email, password, confirmPassword }
       },
-      context,
-      info
     ) {
       //TODO: validate user data
       //todo: make sure user doesn't already exist
+        const user = await User.findOne({ username })
+        if(user){
+            throw new UserInputError('Username is taken', {
+                errors: {
+                    username: 'This username is taken'
+                }
+            })    
+        }
+
       //todo: hash password and create an auth token
       password = await bcrypt.hash(password, 12)
 
