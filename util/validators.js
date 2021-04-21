b256122f74f8097cb67711aa02c8b989a09c8ce5
Sophia Seremetis <shseremetis@gmail.com>
2021-04-21 14:09:08 -0400

    add mutation for user login

--- util/validators.js
+++ util/validators.js
@@ -1,29 +1,44 @@
 module.exports.validateRegisterInput = (
-    username,
-    email, 
-    password, 
-    confirmPassword 
+  username,
+  email,
+  password,
+  confirmPassword
 ) => {
-    const errors = {}
-    if(username.trim()  === ''){
-        errors.username = 'Username must not be empty'
-    }  
-    if(email.trim()  === ''){
-        errors.username = 'Email must not be empty'
-    } else {
-        const regEx = /^[-!#$%&'*+\/0-9=?A-Z^_a-z`{|}~](\.?[-!#$%&'*+\/0-9=?A-Z^_a-z`{|}~])*@[a-zA-Z0-9](-*\.?[a-zA-Z0-9])*\.[a-zA-Z](-?[a-zA-Z0-9])+$/;
-        if(!email.match(regEx)){
-            errors.email = 'Email must be a valid email address'
-        }
+  const errors = {};
+  if (username.trim() === "") {
+    errors.username = "Username must not be empty";
+  }
+  if (email.trim() === "") {
+    errors.username = "Email must not be empty";
+  } else {
+    const regEx = /^[-!#$%&'*+\/0-9=?A-Z^_a-z`{|}~](\.?[-!#$%&'*+\/0-9=?A-Z^_a-z`{|}~])*@[a-zA-Z0-9](-*\.?[a-zA-Z0-9])*\.[a-zA-Z](-?[a-zA-Z0-9])+$/;
+    if (!email.match(regEx)) {
+      errors.email = "Email must be a valid email address";
     }
-    if(password === ''){
-        errors.password = 'Password must not be empty'
-    } else if(password !== confirmPassword){
-        errors.confirmPassword = 'Password must match'
-    } 
+  }
+  if (password === "") {
+    errors.password = "Password must not be empty";
+  } else if (password !== confirmPassword) {
+    errors.confirmPassword = "Password must match";
+  }
 
-    return {
-        errors, 
-        valid: Object.keys(errors).length < 1
-    }
-}
+  return {
+    errors,
+    valid: Object.keys(errors).length < 1,
+  };
+};
+
+module.exports.validateLoginInput = (username, password) => {
+  const errors = {};
+  if (username.trim() === "") {
+    errors.username = "Username must not be empty";
+  }
+  if (password.trim() === "") {
+    errors.password = "Password must not be empty";
+  }
+
+  return {
+    errors,
+    valid: Object.keys(errors).length < 1,
+  };
+};
