a48a0f171a3fc96377775a4c43e46eb3a05b84ef
Sophia Seremetis <shseremetis@gmail.com>
2021-04-21 14:01:17 -0400

    add validator to users.js

--- graphql/resolvers/users.js
+++ graphql/resolvers/users.js
@@ -1,53 +1,64 @@
-const bcrypt = require('bcryptjs')
-const jwt = require('jsonwebtoken');
-const { UserInputError } = require('apollo-server')
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const { UserInputError } = require("apollo-server");
 
-const { SECRET_KEY } = require('../../config')
+const { validateRegisterInput } = require("../../util/validators");
+const { SECRET_KEY } = require("../../config");
 const User = require("../../models/User");
 
 module.exports = {
   Mutation: {
     async register(
       _,
-      { 
-        registerInput: { username, email, password, confirmPassword }
-      },
+      { registerInput: { username, email, password, confirmPassword } }
     ) {
-      //TODO: validate user data
-      //todo: make sure user doesn't already exist
-        const user = await User.findOne({ username })
-        if(user){
-            throw new UserInputError('Username is taken', {
-                errors: {
-                    username: 'This username is taken'
-                }
-            })    
-        }
-
-      //todo: hash password and create an auth token
-      password = await bcrypt.hash(password, 12)
-
-      const newUser = new User({
-          email,
-          username, 
-          password, 
-          createdAt: new Date().toISOString()
-      })
-
-      const res = await newUser.save()
-
-      const token = jwt.sign({
-          id: res.id,
-          email: res.email, 
-          username: res.username
-      }, SECRET_KEY, { expiresIn: '1h'})
-
-      return {
-          ...res._doc, 
-          id: res._id, 
-          token
+      //validate user data
+      const { valid, errors } = validateRegisterInput(
+        username,
+        email,
+        password,
+        confirmPassword
+      );
+      if (!valid) {
+        throw new UserInputError("Errors", { errors });
+      }
+      //make sure user doesn't already exist
+      const user = await User.findOne({ username });
+      if (user) {
+        throw new UserInputError("Username is taken", {
+          errors: {
+            username: "This username is taken",
+          },
+        });
       }
 
+      //hash password and create an auth token
+      password = await bcrypt.hash(password, 12);
+
+      const newUser = new User({
+        email,
+        username,
+        password,
+        createdAt: new Date().toISOString(),
+      });
+
+      const res = await newUser.save();
+
+      const token = jwt.sign(
+        {
+          id: res.id,
+          email: res.email,
+          username: res.username,
+        },
+        SECRET_KEY,
+        { expiresIn: "1h" }
+      );
+
+      return {
+        ...res._doc,
+        id: res._id,
+        token,
+      };
     },
   },
 };
