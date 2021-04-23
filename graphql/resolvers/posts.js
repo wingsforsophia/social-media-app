bcd02ffd9b5755d23fd6a2287a5733749fa502a0
Sophia Seremetis <shseremetis@gmail.com>
2021-04-23 12:15:25 -0400

    add create post mutation

--- graphql/resolvers/posts.js
+++ graphql/resolvers/posts.js
@@ -1,4 +1,5 @@
 const Post = require("../../models/Post")
+const checkAuth = require('../../util/check-auth')
 
 module.exports = {
     Query: {
@@ -25,7 +26,19 @@ module.exports = {
     },
     Mutation: {
         async createPost(_, { body }, context){
-            
+            const user = checkAuth(context)
+            console.log(user)
+
+            const newPost = new Post({
+                body, 
+                user: user.id,
+                username: user.username,
+                createdAt: new Date().toISOString()
+            })
+
+            const post = await newPost.save()
+
+            return post
         }
     }
 }
