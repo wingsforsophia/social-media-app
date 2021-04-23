fed4ba405012db198b3842d4dcf9e370a2fe7c79
Sophia Seremetis <shseremetis@gmail.com>
2021-04-23 12:29:18 -0400

    fixed check auth problems and changed posts to sort by most recently created to oldedst

--- util/check-auth.js
+++ util/check-auth.js
@@ -5,13 +5,13 @@ const { SECRET_KEY } = require('../config')
 
 module.exports = (context) => {
     // context = { ... headers}
-    const authHeader  = context.req.heders.authorization
+    const authHeader  = context.req.headers.authorization
     if (authHeader) {
         // bearer ....
         const token = authHeader.split('Bearer ')[1]
         if (token) {
             try{
-                const user = jwt.verify(toke, SECRET_KEY)
+                const user = jwt.verify(token, SECRET_KEY)
                 return user
             } catch(err){
                 throw new AuthenticationError('Invalid/Expired token')
@@ -19,5 +19,5 @@ module.exports = (context) => {
         }
         throw new Error('Authentication token must be \'Bearer [token]') 
     }
-    throw new Error('Authentication header must be provided')
+    throw new Error('Authorization header must be provided')
 } 
